f87a31726ac30cd597cec24adc32efd4933cee99
Kyoshiro Kamura <reallove1970@gmail.com>
2019-06-05 17:27:23 +0900

    modi itdandild

--- ildloop.m
+++ ildloop.m
@@ -8,12 +8,19 @@ L = 5*Fs;
 noise = GenNoiseWave(Fs*T, 1);
 
 sig = zeros(Fs*T, 2);
-sig(:, 1) = noise(:);
+sig(:, 1) = noise .* 0.6;
 
-w = 2 * triang(L)';
+w = triang(L)';
 
 for i = 1:(Fs*T)/L
     sig(i*L-L+1:i*L, 2) = noise(i*L-L+1:i*L) .* w;
     tmp(i,:) = [i*L-L+1:i*L];
 end
 sound(sig, Fs)
+
+
+figure(1)
+plot(sig(:,1))
+hold on
+plot(sig(:,2))
+ylim([-1.0 1.0])
