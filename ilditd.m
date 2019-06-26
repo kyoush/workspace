7635c312e47a2bf41f75d6e4040c0254c81b9d5e
Kyoshiro Kamura <reallove1970@gmail.com>
2019-06-26 18:44:06 +0900

    complete gui

--- ilditd.m
+++ ilditd.m
@@ -1,51 +1,38 @@
-function sig2 = ilditd(db, delta, ax)
+function sig = ilditd(db, delta, ax)
 [noise, Fs] = audioread('noise.wav');
 T = 1;
-
-%% ild
-sig = zeros(Fs*T, 2);
-% powersig = bandpower(noise) ./ (10^(abs(db)/20));
-% down = power2const(powersig);
-down = power2const(abs(db));
-
-if db > 0
-    sig(:, 1) = noise;
-    sig(:, 2) = noise .* down;
-else
-    sig(:, 1) = noise .* down;
-    sig(:, 2) = noise;
-end
-
-%% itd
 tau = round(abs(delta) * Fs * 0.000001);
-sig2 = zeros(Fs*T + tau, 2);
-
+sig = zeros(Fs*T + tau, 2);
+%% ITD
 if delta > 0
-    sig2(1:Fs*T, 1) = sig(:, 1);
-    sig2(tau+1:end, 2) = sig(:, 2);
+    sig(1:Fs*T, 1) = noise;
+    sig(tau+1:end, 2) = noise;
 else
-    sig2(tau+1:end, 1) = sig(:, 1);
-    sig2(1:Fs*T, 2) = sig(:, 2);
+    sig(tau+1:end, 1) = noise; 
+    sig(1:Fs*T, 2) = noise;
 end
-sig = sig2;
+
+%% ILD
+down = power2const(abs(db));
+if db > 0
+    sig(:, 2) = sig(:, 2) .* down;
+else
+    sig(:, 1) = sig(:, 1) .* down;
+end
 sound(sig, Fs)
 
 %% dB calc
-% l_pow = rms(sig(:, 1));
-% r_pow = rms(sig(:, 2));
-
 l_pow = bandpower(sig(:, 1));
 r_pow = bandpower(sig(:, 2));
-
 a = 20 * log10(l_pow/r_pow);
 
 %% plot
 i = 3000:3500;
 i = i';
 
-h1 = plot(ax, i, sig2(i, 1), 'b');
+h1 = plot(ax, i, sig(i, 1), 'b');
 hold(ax, 'on')
-h2 = plot(ax, i, sig2(i, 2), 'r');
+h2 = plot(ax, i, sig(i, 2), 'r');
 legend(ax, [h1, h2], 'L-channel', 'R-channel', 'location', 'southwest')
 
 if db < 0
@@ -72,5 +59,9 @@ text(ax, 3350, -0.75, str2, 'Color', 'k', 'fontsize', 16);
 
 hold(ax, 'off')
 save('temp.mat');
+
+    function const = power2const(x)
+        const = exp(-0.05756*x);
+    end
 
 end
